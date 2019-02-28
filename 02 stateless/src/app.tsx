8cf9555e4bcca3bcc85a4614c8f831d6954d5cbd
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-28 08:45:11 +0100

    Create stateless React components using TypeScript

---
+++ 02 stateless/src/app.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+type AppProps = {
+  message: string,
+}
+
+const App: React.SFC<AppProps> = ({ message }) => <div>{message}</div>
+
+ReactDOM.render(
+  <App message="Hello world"/>,
+  document.getElementById('root')
+)
